6c2a44a701e9e05acaef4fbe3af8f0c31dbbf431
Information School <ischoolit@uw.edu>
2015-01-26 11:29:20 -0800

    class over for real this time

--- lab2.sql
+++ lab2.sql
@@ -10,8 +10,8 @@ FROM tblEMPLOYEE e
 		ON l.LevelID = es.LevelID
 	JOIN tblSKILL s
 		ON s.SkillID = es.SkillID
-WHERE (s.SkillName = 'Percussion' OR s.SkillName = 'Piano Education' OR s.SkillName = 'Sales')
-	  AND (e.EmpState LIKE '%Michigan%' OR e.EmpState LIKE '%Georgia%');
+WHERE (s.SkillName = 'Percussion' OR s.SkillName = 'Piano Education' OR s.SkillName = 'Sales') AND
+	  (e.EmpState LIKE '%Michigan%' OR e.EmpState LIKE '%Georgia%');
 
 --2) Write a query that returns the 3 employees that have sold the most accessories by dollar volume during December 2012.
 SELECT TOP 3 e.EmpFName, e.EmpLName, SUM(li.Qty * pv.Price) AS TotalSold
@@ -57,8 +57,8 @@ FROM tblLINE_ITEM li
 		ON v.VendorID = pv.VendorID
 	JOIN tblVENDOR_TYPE vt
 		ON vt.VendorTypeID = v.VendorTypeID
-WHERE vt.VendorTypeName = 'Vintage'
-		AND o.OrderDate >= '2009-02-15'
+WHERE vt.VendorTypeName = 'Vintage'AND
+		o.OrderDate >= '2009-02-15'
 GROUP BY v.VendorID, v.VendorName
 ORDER BY ItemsSold DESC;
 
@@ -73,8 +73,14 @@ FROM tblPRODUCT p
 		ON o.OrderID = li.OrderID
 	JOIN tblCUSTOMER c
 		ON c.CustID = o.CustID
-WHERE c.CustState LIKE '%California%'
-		AND o.OrderDate BETWEEN '2006-01-01' and '2006-12-31'
+	JOIN tblPRODUCT_TYPE pt
+		ON pt.ProductTypeID = p.ProductTypeID
+WHERE pt.ProductTypeName = 'Clothing' AND
+		p.ProductName LIKE '%T-Shirt%' AND
+		c.CustState LIKE '%California%' AND
+		o.OrderDate BETWEEN '2006-01-01' and '2006-12-31'
 GROUP BY p.ProductID, p.ProductName
-HAVING SUM(li.Qty) > 3
+HAVING SUM(li.Qty) >= 3
 ORDER BY ItemsSold DESC;
+
+SELECT * FROM tblPRODUCT p JOIN tblPRODUCT_TYPE pt ON pt.ProductTypeID = p.ProductTypeID WHERE pt.ProductTypeName = 'Clothing';
